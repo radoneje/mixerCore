c5534ba10bec76386be794d894446a21ae5ade94
radoneje <den.shevchenko@gmail.com>
2022-04-23 21:15:17 +0300

    1

--- CHttp.cpp
+++ CHttp.cpp
@@ -10,10 +10,18 @@
 #include <mutex>
 
 #include "CHttp.h"
+#include "httplib.h"
+
 CHttp::CHttp(){
 
 
 };
 void CHttp::init(int port){
     std::cout<< "http CHttp"<<std::endl;
+    httplib::Server svr;
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", port);
 }
