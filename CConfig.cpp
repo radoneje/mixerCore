743a8f88412243f3dcf50e0d3047f57c1d4a9f12
radoneje <den.shevchenko@gmail.com>
2022-04-25 12:32:39 +0300

    1

--- CConfig.cpp
+++ CConfig.cpp
@@ -12,28 +12,15 @@
 #include "CConfig.h"
 #include <libconfig.h++>
 
+using namespace libconfig;
 
 CConfig::CConfig() {};
 
 int CConfig::GetConfig(const char *name, const char** str) {
-    config_t cfg;
-    config_setting_t *setting;
 
+    Config cfg;
 
-    config_init(&cfg);
-    if(! config_read_file(&cfg, "/etc/MixerCore/mixerCore.conf"))
-    {
-        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),
-                config_error_line(&cfg), config_error_text(&cfg));
-        config_destroy(&cfg);
-        return(EXIT_FAILURE);
-    }
-    if(config_lookup_string(&cfg, name, str))
-        printf("Store name: %s\n\n", str);
-    else
-        fprintf(stderr, "No 'name' setting in configuration file.\n");
+   // if(! config_read_file(&cfg, "/etc/MixerCore/mixerCore.conf"))
 
-    config_destroy(&cfg); /* Освободить память обязательно, если это не конец программы */
-    return(EXIT_SUCCESS);
 }
 
