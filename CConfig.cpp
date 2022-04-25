21828232232c28cdcf422f0ab8113e1ec325d7ff
radoneje <den.shevchenko@gmail.com>
2022-04-25 13:31:06 +0300

    1

--- CConfig.cpp
+++ CConfig.cpp
@@ -20,8 +20,8 @@ CConfig::CConfig() {};
 
 int CConfig::GetConfig(const char *fileNname, const char** str) {
 
-    std::ifstream cFile();
-    cFile.open("/etc/MixerCore/mixerCore.conf" );
+    std::ifstream cFile("/etc/MixerCore/mixerCore.conf");
+
    // if(! config_read_file(&cfg, "/etc/MixerCore/mixerCore.conf"))
     if (cFile.is_open())
     {
