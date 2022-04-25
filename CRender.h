f30d3252b1e91ac24823c24b777d73001cfcd50d
radoneje <den.shevchenko@gmail.com>
2022-04-25 23:39:03 +0300

    1

--- CRender.h
+++ CRender.h
@@ -6,19 +6,19 @@
 #define MIXERCORE_CRENDER_H
 
 
+struct sTextureImage {
+public:
+    unsigned char *pixels;
+    int width;
+    int height;
+    int numChannels;
 
+};
 class CRender {
 private:
     static void Reshape(int width, int height);
     static void  Display();
-    static struct sTextureImage {
-    public:
-        unsigned char *pixels;
-        int width;
-        int height;
-        int numChannels;
 
-    };
     void ReadPPMImage (char *fileName, sTextureImage &textureImage );
     static sTextureImage *textureImage= new sTextureImage[8];
     static void ReadPPMImage (char *fileName);
