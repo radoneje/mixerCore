a14d7ce821b75d63a076739eed83206c4a786970
radoneje <den.shevchenko@gmail.com>
2022-04-25 12:27:24 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -17,7 +17,8 @@
     void CRender::StartRender(){
         std::cout << "start render" << std::endl;
         const char *str;
-        const char name[]="name";
+        const char *name;
+        name="name";
         CConfig::GetConfig(name,  &str);
       /*  glutInit(&argc, argv);
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
