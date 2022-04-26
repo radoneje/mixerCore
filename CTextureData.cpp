35da4b18956e72c2f23b876eeb0f091dfad1009a
radoneje <den.shevchenko@gmail.com>
2022-04-26 11:30:04 +0300

    1

--- CTextureData.cpp
+++ CTextureData.cpp
@@ -1,11 +1,20 @@
 //
 // Created by Denis Shevchenko on 26.04.2022.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <mutex>
 
 #include "CTextureData.h"
 
 CTextureData::CTextureData(){
+    std::cout<< "CTextureData()"<< std::endl;
     this->width=0;
     this->height=0;
     this->numChannels=0;
+
 }
