d5dd4bf405767135162a9b1130149e669edd122c
radoneje <den.shevchenko@gmail.com>
2022-04-28 00:04:14 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -26,7 +26,7 @@
 
   CTextureData * CRender::textureData[MAX_FACES];
 
-void CRender::ReadPPMImage(  std::string *fileName, CTextureData *textureDataParam) {
+void CRender::ReadPPMImage( const std::string  &fileName, CTextureData *textureDataParam) {
   /*  int tmpint;
     char str[100];
     FILE* inFile = fopen (fileName,"rb");
@@ -108,7 +108,7 @@ void CRender::Display(){
         snprintf(buf, sizeof(buf), "/var/www/video-broadcast.space/102.ppm");
         std::string filename;
         filename="/var/www/video-broadcast.space/102.ppm";
-        ReadPPMImage( &filename , textureData[0]);
+        ReadPPMImage( "/var/www/video-broadcast.space/102.ppm" , textureData[0]);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureData[i]->width,
                      textureData[i]->height, 0, GL_RGB, GL_UNSIGNED_BYTE,
                      textureData[i]->pixels);
