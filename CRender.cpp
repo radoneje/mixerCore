461cb324a7e4eeb581b08b6ed5b2447f635d2e05
radoneje <den.shevchenko@gmail.com>
2022-04-28 01:03:19 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -86,6 +86,7 @@ void CRender::ReadPPMImage( const char* fileName, CTextureData *textureDataParam
 
 }
 
+  Image CRender::image;
 void CRender::ReadPPMImage2( const char* fn, CTextureData &textureDataParam) {
 
 
