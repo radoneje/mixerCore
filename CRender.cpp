56671ccba28995a7f68f9ecd0cecacdfe1be9c44
radoneje <den.shevchenko@gmail.com>
2022-04-28 18:09:58 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -213,13 +213,13 @@ void CRender::Display() {
 
     glBegin(GL_QUADS);
         glTexCoord2f(0, 0);
-        glVertex3f(-1, 1, -5);
+        glVertex3f(-1, 1, -8);
         glTexCoord2f(0, 1);
-        glVertex3f(-1, -1, -5);
+        glVertex3f(-1, -1, -8);
         glTexCoord2f(1, 1);
-        glVertex3f(1, -1, -5);
+        glVertex3f(1, -1, -8);
         glTexCoord2f(1, 0);
-        glVertex3f(1, 1, -5);
+        glVertex3f(1, 1, -8);
     glEnd();
     glDisable(GL_TEXTURE_2D);
 
