45f4bbaebda706b6eb1e5a1fb36f16b55bb32f45
radoneje <den.shevchenko@gmail.com>
2022-04-28 12:51:43 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -162,10 +162,11 @@ void CRender::Display(){
 
         for(int i=0; i<MAX_FACES; i++) {
             sImage item;
-            item.xRight=1.0f *0.25;
-            item.yTop=1.0f *0.25;
-            item.xLeft=-1.0f *0.25;
-            item.yBottom=-1.0f *0.25;
+            item.xLeft=-1.0f + (i*0.25);
+            item.xRight=-1.0f + (i*0.25)+0.25;
+            item.yTop=1.0f;
+
+            item.yBottom=-1.0f;
 
             texturePlaceholder.push_back(item);
             std::cout<<  item.xLeft<<"\t" << item.yTop<<std::endl;
