27e921facd8b3af7a48d69b67c40587e129376a7
radoneje <den.shevchenko@gmail.com>
2022-04-28 16:17:50 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -139,6 +139,7 @@ void CRender::Display() {
         if(cell>=4) {
             cell = 0;
             row++;
+
         }
 
 
@@ -153,30 +154,30 @@ void CRender::Display() {
         if(row==0) {
             glColor3d(1, 0, 0);
             glTexCoord2f(0, 0);
-            glVertex3f(0, 1 + 0.5, -8);
+            glVertex3f(-1 + (cell * 0.5), -1 + 0.5, -8);
 
             glTexCoord2f(0, 1);
             glColor3d(1, 1, 0);
-            glVertex3f(0 , 0, -8);
+            glVertex3f(-1 + (cell * 0.5), -1, -8);
 
             glTexCoord2f(1, 1);
             glColor3d(1, 1, 1);
-            glVertex3f(1 , 0, -8);
+            glVertex3f(-1 + 0.5 + (cell * 0.5), -1, -8);
 
             glTexCoord2f(1, 0);
             glColor3d(0, 1, 1);
-            glVertex3f(1, 1, -8);
+            glVertex3f(-1 + 0.5 + (cell * 0.5), -1 + 0.5, -8);
         }
         else
         {
 
             glColor3d(1, 0, 0);
             glTexCoord2f(0, 0);
-            glVertex3f(1 - 0.5, -1 + 0.5, -8);
+            glVertex3f(-1, 1, -8);
 
             glTexCoord2f(0, 1);
             glColor3d(1, 1, 0);
-            glVertex3f(1 -  0.5, -1, -8);
+            glVertex3f(-1, -1, -8);
 
             glTexCoord2f(1, 1);
             glColor3d(1, 1, 1);
@@ -184,7 +185,7 @@ void CRender::Display() {
 
             glTexCoord2f(1, 0);
             glColor3d(0, 1, 1);
-            glVertex3f(1, -1 + 0.5, -8);
+            glVertex3f(1, 1, -8);
         }
         glEnd();
         glDisable(GL_TEXTURE_2D);
