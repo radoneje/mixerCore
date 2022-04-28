2502fbe3febbcd7e4a91da4491c3bfbc5c54eee4
radoneje <den.shevchenko@gmail.com>
2022-04-28 15:13:02 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -136,24 +136,15 @@ void CRender::Display(){
 
     glBegin(GL_QUADS);
         glColor3d(1,0,0);
-        glVertex3f(-0.1,-0.1,-8);
+        glVertex3f(-0.3,-0.1,-8);
         glColor3d(1,1,0);
-        glVertex3f(0.1,-0.1,-8);
+        glVertex3f(0,-0.3,-8);
         glColor3d(1,1,1);
-        glVertex3f(0.1,0.1,-8);
+        glVertex3f(0,0,-8);
         glColor3d(0,1,1);
-        glVertex3f(-0.1,0.1,-8);
+        glVertex3f(-0.3,0,-8);
     glEnd();
-    glBegin(GL_QUADS);
-        glColor3d(1,0,0);
-        glVertex3f(-1,-0.1,-8);
-        glColor3d(1,1,0);
-        glVertex3f(0.1,-0.1,-8);
-        glColor3d(1,1,1);
-        glVertex3f(0.1,0.1,-8);
-        glColor3d(0,1,1);
-        glVertex3f(-1,0.1,-8);
-    glEnd();
+
     glutSwapBuffers();
     return;
 
