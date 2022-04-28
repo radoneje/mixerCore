52ddefdd2fc289a9121b7d7d0a6384b1f73f4210
radoneje <den.shevchenko@gmail.com>
2022-04-28 14:45:58 +0300

    1

--- CRender.h
+++ CRender.h
@@ -55,7 +55,7 @@ private:
 
     static void Reshape(int width, int height);
     static void  Display();
-    static const int MAX_FACES=1;
+    static const int MAX_FACES=4;
    // static GLuint textures;
     static GLuint textures[MAX_FACES];
     static  CTextureData textureData2;
