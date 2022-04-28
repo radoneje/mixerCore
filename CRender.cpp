3051028813aeba79f3e116e025be803a5b77c475
radoneje <den.shevchenko@gmail.com>
2022-04-28 12:55:44 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -162,8 +162,8 @@ void CRender::Display(){
 
         for(int i=0; i<MAX_FACES; i++) {
             sImage item;
-            item.xLeft=-1.0f;// + (i*0.25);
-            item.xRight=-1.0f +0.5 ;//+ (i*0.25)+0.25;
+            item.xLeft=-1.0f + (i*0.5);// + (i*0.25);
+            item.xRight=-1.0f + (i*0.5) +0.5 ;//+ (i*0.25)+0.25;
 
             item.yTop=1.0f*0.25;
             item.yBottom=-1.0f*0.25;
