e18e4d4493431ef36079daa36cd028ded450bf5f
radoneje <den.shevchenko@gmail.com>
2022-04-28 13:39:50 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -105,7 +105,7 @@ void CRender::Reshape(int width, int height){
         glPixelStorei (GL_UNPACK_ALIGNMENT, 1);
 
         for(int i=0; i<texturePlaceholder.size(); i++) {
-            glActiveTexture(GL_TEXTURE0);
+          //  glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_2D, 1);
             std::cout<<textures[i]<< "<--textures[i]"<<std::endl;
 
