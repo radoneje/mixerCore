1b8b07839ee1fccaeedb7a70c8b2e03bfb1ad534
radoneje <den.shevchenko@gmail.com>
2022-04-28 11:52:50 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -108,9 +108,9 @@ void CRender::Reshape(int width, int height){
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width,
-                     image.height, 0, GL_RGB, GL_UNSIGNED_BYTE,
-                     image.pixels);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texturePlaceholder.width,
+                     texturePlaceholder.height, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                     texturePlaceholder.pixels);
     }
     glViewport(0, 0, width, height);
 
