3b3ca4c2a44c01a7eb5c6368f074df2a6690f3b0
radoneje <den.shevchenko@gmail.com>
2022-04-28 22:54:08 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -228,6 +228,27 @@ void CRender::Display() {
     if(pCmd->activeTextureId.size()==1)
     {
         std::cout<<"render active cmd"<< pCmd->activeTextureId[0] <<std::endl;
+        glEnable(GL_TEXTURE_2D);
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texturePlaceholder[pCmd->activeTextureId[0]].width,
+                     texturePlaceholder[pCmd->activeTextureId[0]].height, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                     texturePlaceholder[pCmd->activeTextureId[0]].pixels);
+
+
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0);
+        glVertex3f(-1, 0.5, -8);
+        glTexCoord2f(0, 1);
+        glVertex3f(-1, -1, -8);
+        glTexCoord2f(1, 1);
+        glVertex3f(0.5, -1, -8);
+        glTexCoord2f(1, 0);
+        glVertex3f(0.5, 0.5, -8);
+        glEnd();
+        glDisable(GL_TEXTURE_2D);
 
     }
 
