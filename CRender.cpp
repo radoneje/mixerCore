331f8dac2930a24707a488d14a609690f9dc5fde
radoneje <den.shevchenko@gmail.com>
2022-04-28 13:14:56 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -31,6 +31,7 @@ CTextureData  CRender::textureData2;
 //sImage CRender::texturePlaceholder;
 std::vector<sImage> CRender::texturePlaceholder;
 std::vector<std::vector<float>>  CRender::texturePosition(MAX_FACES, std::vector<float>(2));
+GLuint CRender::textures[MAX_FACES];
 
 void CRender::ReadPPMImage2( const char* fileName, sImage *image) {
 
