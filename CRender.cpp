3230f4ff282f656f664eccfff27c4f2bb197c005
radoneje <den.shevchenko@gmail.com>
2022-04-29 11:37:00 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -230,7 +230,7 @@ void CRender::Idle() {
         std::lock_guard<std::mutex> lockGuard(pCmd->locker);
        // pCmd->locker.lock();
         if (pCmd->activeTextureId.size() == 1) {
-            std::cout << "render active cmd " << pCmd->activeTextureId[0] << " " << std::endl;
+           // std::cout << "render active cmd " << pCmd->activeTextureId[0] << " " << std::endl;
             glEnable(GL_TEXTURE_2D);
             glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
             glBindTexture(GL_TEXTURE_2D, textures[0]);
