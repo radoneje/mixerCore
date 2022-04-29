ca90d673fa1c806ebd8963213ef13f854b29d259
radoneje <den.shevchenko@gmail.com>
2022-04-29 10:42:13 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -139,7 +139,7 @@ void CRender::Reshape(int width, int height){
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
-void CRender::Display() {
+void CRender::Display() {}
 void CRender::Idle() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     int cell=0, row=0;
