57941f0fe4aa42be0b262fe04c9eab28a31d9cb1
radoneje <den.shevchenko@gmail.com>
2022-05-06 16:23:52 +0300

    1

--- CHttp.cpp
+++ CHttp.cpp
@@ -20,11 +20,12 @@ CHttp::CHttp(){
 void CHttp::init(int port, Ccmd *pCmd){
     std::cout<< "http CHttp: "<< port <<std::endl;
     httplib::Server svr;
-    svr.Post("/mixer/activatePresImg",[&](const httplib::Request &req, httplib::Response &res){
+    svr.Post(R"(/mixer/activatePresImg/(.+))",[&](const httplib::Request &req, httplib::Response &res){
        std::cout<< "/mixer/activatePresImg" << std::endl;
+        const std::string eventid = req.matches[1];
         auto ret = req.has_file("image");
         const auto& file = req.get_file_value("image");
-        const auto eventid =req.get_param_value("eventid");
+       // const auto eventid =req.get_param_value("eventid");
         std::cout<< file.filename << " " << eventid << std::endl;
 
         std::ofstream myfile;
