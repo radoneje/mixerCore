ea046ecb84608a5c6b2273b913fe4b625e788e42
radoneje <den.shevchenko@gmail.com>
2022-05-06 20:16:54 +0300

    1

--- Ccmd.cpp
+++ Ccmd.cpp
@@ -25,6 +25,7 @@ Ccmd::Ccmd(){
 void Ccmd::startReadStream(std::string rtmpURL, int layerNumber ){
     auto worker=FFreader[layerNumber];
     std::cout<< "start input " << rtmpURL<<" " << layerNumber<< std::endl;
+    worker.work(rtmpURL);
 
 }
 void Ccmd::loadPresImage(std::string filepath, const std::string simageid){
