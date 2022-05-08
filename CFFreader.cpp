7b65c9cf90cbdd83990c4e504dadd0c1a57279fc
radoneje <den.shevchenko@gmail.com>
2022-05-09 00:06:12 +0300

    1

--- CFFreader.cpp
+++ CFFreader.cpp
@@ -109,7 +109,7 @@ void CFFreader::work(const std::string url, Data  *pData, std::mutex *pLocker){/
 
     if (avcodec_parameters_to_context(ctx_codec, vid_stream->codecpar) < 0)
         std::cout << 512 << ctx_codec->pix_fmt << std::endl;
-   // ctx_codec->hwaccel = ff_find_hwaccel(ctx_codec->codec->id, ctx_codec->pix_fmt);
+    ctx_codec->hwaccel = ff_find_hwaccel(ctx_codec->codec->id, ctx_codec->pix_fmt);
 
     std::cout << 514 << ctx_codec->hwaccel << std::endl;
 
