b026f87fe2a81468dc2aa1b682d958055796a770
radoneje <den.shevchenko@gmail.com>
2022-05-09 13:40:09 +0300

    1

--- CRender.cpp
+++ CRender.cpp
@@ -161,7 +161,7 @@ void CRender::Idle() {
         {
             std::lock_guard<std::mutex> lockGuard(pCmd->locker);
            // std::cout<< "pCmd->FFreader[i].dt.width"<< pCmd->FFreader[i].dt.width<<std::endl;
-            if (false && pCmd->FFreader[i].dt.width <240 ||pCmd->FFreader[i].dt.width>1920) { // затычка
+            if (true || pCmd->FFreader[i].dt.width <240 ||pCmd->FFreader[i].dt.width>1920) { // затычка
 
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texturePlaceholder[i].width,
                              texturePlaceholder[i].height, 0, GL_RGB, GL_UNSIGNED_BYTE,
