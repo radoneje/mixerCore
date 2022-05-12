41280f87f3d47edddab76e7843320dca45742ba2
radoneje <den.shevchenko@gmail.com>
2022-05-12 19:03:54 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -211,7 +211,7 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
             av_usleep(dts - now);
         }
         int got_packet=0;
-        ret = avcodec_encode_video2(c, &pkt, frame, &got_packet);{
+        ret = avcodec_encode_video2(c, pkt, frame, &got_packet);{
             fprintf(stderr, " Error avcodec_encode_video2 \n",);
             exit(1);
         }
