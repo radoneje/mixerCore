e018e3cb8d729cae4e44805b45f1b6c854beb63f
radoneje <den.shevchenko@gmail.com>
2022-05-12 23:31:12 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -144,7 +144,7 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
     /* open it */
 
 
-    av_dict_set(&opts, "preset", "superfast", 0);
+    av_dict_set(&opts, "preset", "fast", 0);
     av_dict_set(&opts, "tune", "zerolatency", 0);
     av_dict_set(&opts, "profile", "hi", 0);
     av_dict_set(&opts, "lavel", "4.0", 0);
