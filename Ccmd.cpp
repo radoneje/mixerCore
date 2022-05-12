c00129fcea2dd09c1ad38862fb2ff26ee1ba4cdb
radoneje <den.shevchenko@gmail.com>
2022-05-12 11:25:48 +0300

    1

--- Ccmd.cpp
+++ Ccmd.cpp
@@ -102,6 +102,7 @@ int Ccmd::startStream(const std::string eventid){
    CffmpegStreamer *streamer= new  CffmpegStreamer();
    // streamers.insert(std::pair<std::string, CffmpegStreamer*> ( eventid, streamer ));
     std::thread streamThread(CffmpegStreamer::startStream, eventid);
+    streamThread.join();
 
     return 0;
 }
