b8608928e61786be38dc0ee95d1c5f0cd97d8afc
radoneje <den.shevchenko@gmail.com>
2022-05-12 11:46:23 +0300

    1

--- Ccmd.h
+++ Ccmd.h
@@ -17,7 +17,7 @@ class Ccmd {
 
     public:
         std::map<std::string, CffmpegStreamer*> streamers;
-        int startStream(const std::string eventid);
+        int startStream(const std::string eventid, unsigned char * data);
         std::vector<CFFreader>  FFreader;
         std::vector<int> activeTextureId;
         std::mutex locker;
