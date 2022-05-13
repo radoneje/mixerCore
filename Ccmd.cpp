bf4a13e9a5a16b0a814735f7a8ccb83998ea6140
radoneje <den.shevchenko@gmail.com>
2022-05-13 18:33:38 +0300

    1

--- Ccmd.cpp
+++ Ccmd.cpp
@@ -152,7 +152,7 @@ int Ccmd::startStream(const std::string eventid){
 
     printf("startStream\n");
 
-    std::thread streamThread(CffmpegStreamer::startStream, eventid, mainImageData  (std::function<void(std::string)>) notifyStreamStarted,  (std::function<void(std::string)>) notifyStreamEnded);
+    std::thread streamThread(CffmpegStreamer::startStream, eventid, mainImageData , (std::function<void(std::string)>) notifyStreamStarted,  (std::function<void(std::string)>) notifyStreamEnded);
     streamThread.detach();
 
     SstreamData dt;
