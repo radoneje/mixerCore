d652da08eace75faeadd031cb5e894b5b244a615
radoneje <den.shevchenko@gmail.com>
2022-05-14 02:33:27 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -270,7 +270,7 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
             if(dts<(frame->pts*1000))
             {
                 std::cout<< dts - (frame->pts*1000)<<" sleeo"<<std::endl;
-                av_usleep(dts - (frame->pts*1000));
+                av_usleep((frame->pts*1000)-dts );
             }
            /* AVRational time_base=ofmt_ctx->streams[0]->time_base;
             AVRational time_base_q={1,AV_TIME_BASE};
