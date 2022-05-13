a2fcd860279dc4942fdc905137dcb2d67f30eaa7
radoneje <den.shevchenko@gmail.com>
2022-05-13 22:50:21 +0300

    1

--- Ccmd.cpp
+++ Ccmd.cpp
@@ -72,11 +72,9 @@ Ccmd::makeMainImage(std::string eventid, unsigned char *mainImageData, std::vect
 
         for(int i=0;i< previewImageData.size();i++)
         {
-            std::cout<< "previewImageData.size()"<< i<< std::endl;
             Magick::Image imageInput;
             imageInput.read(WIDTH, HEIGHT, "RGB", MagickLib::CharPixel, previewImageData[i]);
             imageInput.resize( Magick::Geometry(ww, hh));
-
             if(i<4)
                 image.composite(imageInput,ww*i, 0);
             else
