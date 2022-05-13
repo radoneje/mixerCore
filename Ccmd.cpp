fe215528416db6ccef3d954586b57f9b40e28b6a
radoneje <den.shevchenko@gmail.com>
2022-05-13 19:09:11 +0300

    1

--- Ccmd.cpp
+++ Ccmd.cpp
@@ -49,13 +49,14 @@ Ccmd::Ccmd(){
  long long i=0;
  while(true) {
      i++;
-     std::cout<<"==0 render image frame" <<endl;
+
      unsigned char *buf = (unsigned char *) malloc(memorySize);
 
      for (int y = 0; y < HEIGHT; y++)
          for (int x = 0; x < WIDTH; x=x+3) {
              std::cout<<"==1 render image frame" << 1000/FRAMERATE <<endl;
              std::this_thread::sleep_for(std::chrono::milliseconds(1000/FRAMERATE));
+             std::cout<<"==2 render image frame" <<endl;
              buf[x+y+0]=mainImageData[x+y+0];
              buf[x+y+1]=mainImageData[x+y+1];
              buf[x+y+1]=mainImageData[x+y+1];
