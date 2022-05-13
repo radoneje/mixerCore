7a61d078b9bea93a615ac34893b743d54a6a5052
radoneje <den.shevchenko@gmail.com>
2022-05-14 02:48:38 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -264,7 +264,7 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
                 startTime = av_gettime();
             }
             int64_t now_time = av_gettime();// - startTime;
-            int mast=(1000000)/30;
+            int mast=(1000000/30)*frame->pts;
             int  fact = now_time-startTime;
 
             av_usleep( 0);
