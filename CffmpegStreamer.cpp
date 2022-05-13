4a717105413bf9ed256ec74ee560871a2070e382
radoneje <den.shevchenko@gmail.com>
2022-05-14 02:36:37 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -100,7 +100,7 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
     AVDictionary *opts = nullptr;
 
     std::string outUrl="rtmp://wowza02.onevent.online/live/mixerCore1";
-    outUrl="rtmp://a.rtmp.youtube.com/live2/w0wa-hcba-raqz-880p-7v0h";
+   // outUrl="rtmp://a.rtmp.youtube.com/live2/w0wa-hcba-raqz-880p-7v0h";
     //outUrl="/var/www/mixerControl/public/1.mp4";
     std::string codec_name = "libx264";
 
