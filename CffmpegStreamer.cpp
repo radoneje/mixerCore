d7327b6cc9cb1d12614e51f7ead69f49a0c59003
radoneje <den.shevchenko@gmail.com>
2022-05-13 23:49:22 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -114,6 +114,11 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
 
 
     out_stream = avformat_new_stream(ofmt_ctx, NULL);
+    AVDictionary *format_opts = NULL;
+    av_dict_set(&format_opts, "movflags", "faststart+empty_moov", 0);
+
+    ret = avformat_write_header( ofmt_ctx, &format_opts );
+
     if (!out_stream) {
         av_log(NULL, AV_LOG_ERROR, "Failed allocating output stream\n");
         return ;
@@ -152,6 +157,7 @@ void CffmpegStreamer::startStream(const std::string eventid, unsigned char * ima
     av_dict_set(&opts, "tune", "zerolatency", 0);
    // av_dict_set(&opts, "profile", "hi", 0);
     av_dict_set(&opts, "lavel", "4.0", 0);
+
 
     ret = avcodec_open2(enc_ctx, encoder, &opts);
     if (ret < 0) {
