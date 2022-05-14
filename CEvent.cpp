8e9dcdfda476c9fd365bf3a2c96af3bff401e260
radoneje <den.shevchenko@gmail.com>
2022-05-14 23:42:20 +0300

    1

--- CEvent.cpp
+++ CEvent.cpp
@@ -31,6 +31,6 @@ CEvent::CEvent(std::string eventid) {
         // image.crop( Magick::Geometry(0,0, WIDTH, HEIGHT) );
         unsigned char *pixels = (unsigned char *) malloc(CConfig::HEIGHT * CConfig::WIDTH * 3 * sizeof(unsigned char));
         image.write(0, 0, CConfig::WIDTH, CConfig::HEIGHT, "RGB", MagickLib::CharPixel, pixels);
-        previewImageData.insert(pixels);
+        previewImageData.push_back(pixels);
     }
 }
