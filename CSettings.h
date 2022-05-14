9c3bd0fad0c3bcaab561327c9d9c0e9a0c1690ea
radoneje <den.shevchenko@gmail.com>
2022-05-14 19:59:47 +0300

    1

--- CSettings.h
+++ CSettings.h
@@ -5,8 +5,6 @@
 #ifndef MIXERCORE_CSETTINGS_H
 #define MIXERCORE_CSETTINGS_H
 
-#endif //MIXERCORE_CSETTINGS_H
-
 int MAX_FACES=7;
 int WIDTH=1280;
 int HEIGHT=720;
@@ -14,3 +12,6 @@ std::string RTMP_MAIN="rtmp://wowza02.onevent.online/live/";
 int FRAMERATE=30;
 int MIXER_BITRATE=1024*1024*1.5;
 int MIXER_GOP=30;
+
+#endif //MIXERCORE_CSETTINGS_H
+
