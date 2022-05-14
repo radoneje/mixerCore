0e1ebfff344f03c0ef5785321086497aa010422f
radoneje <den.shevchenko@gmail.com>
2022-05-14 20:21:49 +0300

    1

--- CConfig.cpp
+++ CConfig.cpp
@@ -36,6 +36,7 @@ int CConfig::getGlobalValues(){
 
             GetConfig("WIDTH", buf);
             WIDTH= atoi(buf.c_str());
+            log("WIDTH",WIDTH );
 
             GetConfig("HEIGHT", buf);
             HEIGHT= atoi(buf.c_str());
