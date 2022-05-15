041d93cfa8d69145734557bbcb836e3c61c83b38
radoneje <den.shevchenko@gmail.com>
2022-05-15 21:12:58 +0300

    1

--- CEvent.cpp
+++ CEvent.cpp
@@ -19,6 +19,9 @@ CEvent::CEvent(std::string eventid) {
      startTime = std::chrono::system_clock::now().time_since_epoch().count();
     _eventid=eventid;
     stop= false;
+    int imageSize=CConfig::WIDTH*CConfig::HEIGHT*3*sizeof(unsigned char);
+    int previewImageSize=(CConfig::WIDTH/4)*(CConfig::HEIGHT/4)*3*sizeof(unsigned char);
+
     for(int i=0; i<CConfig::MAX_FACES; i++) {
         std::string fileName("/etc/mixerCore/images/notconnected");
         fileName.append(std::to_string(i + 1));
@@ -26,6 +29,9 @@ CEvent::CEvent(std::string eventid) {
         int h = CConfig::HEIGHT;
         int w = CConfig::WIDTH;
         SImageData imageDataItem;
+        imageDataItem.fullImageData= (unsigned char*)malloc(imageSize);
+        imageDataItem.previewImageData= (unsigned char*)malloc(imageSize);
+        imageDataItem.previewImageData=(unsigned char*)malloc(previewImageSize);
         Magick::InitializeMagick(nullptr);
         Magick::Image image;
         image.read(fileName.c_str());
@@ -37,5 +43,7 @@ CEvent::CEvent(std::string eventid) {
         imageData.push_back(imageDataItem);
     }
     SImageData imageDataItem;
+    imageDataItem.previewImageData= (unsigned char*)malloc(imageSize);
+    imageDataItem.previewImageData=(unsigned char*)malloc(previewImageSize);
     imageData.push_back(imageDataItem);
 }
