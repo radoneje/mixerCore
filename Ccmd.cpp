1b0e5ee551600e45c04dcd56565f81341e30cd94
radoneje <den.shevchenko@gmail.com>
2022-05-16 18:39:42 +0300

    1

--- Ccmd.cpp
+++ Ccmd.cpp
@@ -87,11 +87,11 @@ void Ccmd::makeMainImage(std::string eventid,
                         int inputX=x-col*ww;
                     std::cout<<((x + (y * CConfig::WIDTH)) * 3) + 0<< " inputX "  <<(int)((inputX + (y * ww)) * 3) + 0<<std::endl;
                         blankImage[((x + (y * CConfig::WIDTH)) * 3) + 0] =
-                                pEvent->imageData[0].previewImageData[(int)((inputX + (y * ww)) * 3) + 0];
+                                pEvent->imageData[col].previewImageData[(int)((inputX + (y * ww)) * 3) + 0];
                         blankImage[((x + (y * CConfig::WIDTH)) * 3) + 1] =
-                                pEvent->imageData[0].previewImageData[(int)((inputX + (y * ww)) * 3) + 1];
+                                pEvent->imageData[col].previewImageData[(int)((inputX + (y * ww)) * 3) + 1];
                         blankImage[((x + (y * CConfig::WIDTH)) * 3) + 2] =
-                                pEvent->imageData[0].previewImageData[(int)((inputX + (y * ww)) * 3) + 2];
+                                pEvent->imageData[col].previewImageData[(int)((inputX + (y * ww)) * 3) + 2];
                       /*  blankImage[((x + (y * CConfig::WIDTH)) * 3) + 0] =
                                 pEvent->imageData[col].previewImageData[(int)((inputX + (y * ww)) * 3) + 1];
                         blankImage[((x + (y * CConfig::WIDTH)) * 3) + 0] =
