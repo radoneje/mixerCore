83d58c337af18f760b1d3eb609196b4a08cbe5be
radoneje <den.shevchenko@gmail.com>
2022-05-17 18:52:38 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -245,6 +245,8 @@ void CffmpegStreamer::startStream(const std::string eventid, CEvent *pEvent,  st
                 pkt->stream_index = 0;
                 pkt->duration = (1000 / enc_ctx->time_base.den) * (j + 1);
                 pkt->pos = -1;
+                pkt->dts=frame->pts;
+                pkt->dts=frame->pts;
 
                 j = 0;
                 av_interleaved_write_frame(ofmt_ctx, pkt);
