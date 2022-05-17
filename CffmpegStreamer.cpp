c346abe621930633290a302d88e86210cc50bc30
radoneje <den.shevchenko@gmail.com>
2022-05-17 18:55:37 +0300

    1

--- CffmpegStreamer.cpp
+++ CffmpegStreamer.cpp
@@ -243,7 +243,7 @@ void CffmpegStreamer::startStream(const std::string eventid, CEvent *pEvent,  st
                     return;
                 }
                 pkt->stream_index = 0;
-                pkt->duration = (1000 / enc_ctx->time_base.den) * (j + 1);
+             //   pkt->duration = (1000 / enc_ctx->time_base.den) * (j + 1);
                 pkt->pos = -1;
                 pkt->dts=frame->pts;
                 pkt->dts=frame->pts;
